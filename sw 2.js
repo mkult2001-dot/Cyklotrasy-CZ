cd8706f67a330fe1eeb7a7390bddda9bdecb66d0
mkult2001-dot <mkult2001@gmail.com>
2026-08-08 18:21:46 +0200

    Delete sw 2.js

--- sw 2.js
+++
@@ -1,4 +0,0 @@
-const CACHE='cyklotrasy-cz-v2';const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r}).catch(()=>caches.match('./index.html'))))});
